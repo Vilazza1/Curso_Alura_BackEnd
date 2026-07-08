a40573cc50a0a96ae42a62ec27e57bc64cc7a520
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-07-07 22:00:42 -0300

    adiciona lógica de menu interativo na classe Desafio para opções de conta

--- PrimeiroProjeto/src/Desafio.java
+++ PrimeiroProjeto/src/Desafio.java
@@ -1,16 +1,35 @@
 
+import java.util.Scanner;
+
 public class Desafio {
 
     static void main(String[] args) {
         String nome = "Vinicius";
         String tipoConta = "Corrente";
         double saldo = 2000;
+        int opcao = 0;
 
         System.out.println("*****************************");
         System.out.println("\nNome do Conta: " + nome);
         System.out.println("Tipo da Conta: " + tipoConta);
         System.out.println("Saldo da Conta: " + saldo);
         System.out.println("\n*****************************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Fazer PIx
+                3 - Receber valor
+                4 - sair
+                """;
+
+        Scanner ler = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = ler.nextInt();
+
+        }
 
     }
 }
