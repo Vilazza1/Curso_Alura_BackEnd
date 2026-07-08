cd1472782eb51ba70db2f49a6c597e27b2cd72f8
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-07-07 21:55:11 -0300

    adiciona classe Desafio com exibição de informações da conta e classe OutroLoop para calcular média de avaliações de filmes

---
+++ PrimeiroProjeto/src/Desafio.java
@@ -0,0 +1,16 @@
+
+public class Desafio {
+
+    static void main(String[] args) {
+        String nome = "Vinicius";
+        String tipoConta = "Corrente";
+        double saldo = 2000;
+
+        System.out.println("*****************************");
+        System.out.println("\nNome do Conta: " + nome);
+        System.out.println("Tipo da Conta: " + tipoConta);
+        System.out.println("Saldo da Conta: " + saldo);
+        System.out.println("\n*****************************");
+
+    }
+}
