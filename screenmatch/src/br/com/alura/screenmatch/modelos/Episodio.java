8f61c0be6af0c17c3a9bf9c671d896d9b8561229
Vilaza1 <vinicius.vilaca2023@gmail.com>
2026-07-22 21:33:05 -0300

    adiciona a interface Classicavel e implementa no class Filme; adiciona a classe Episodio com atributos e métodos de acesso

---
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Episodio {
+
+    private int numero;
+    private String nome;
+    private Serie serie;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+}
